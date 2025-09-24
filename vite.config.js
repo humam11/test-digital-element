7eaef421ec169208718c5321be5b6fe688a768ca
humam11 <humamkhalilhk@hotmail.com>
2025-09-25 00:19:17 +0500

    new commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/test-digital-element/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        contact: resolve(process.cwd(), 'contact-us.html')
+      }
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
