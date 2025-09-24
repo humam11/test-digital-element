7a64b5ac0bdf93ddef7009c86514259b7498f913
humam11 <humamkhalilhk@hotmail.com>
2025-09-25 00:22:52 +0500

    new commit

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -10,12 +10,12 @@ export function createHeader(currentPage = "index") {
   
   header.innerHTML = `
     <a href="#" class="logo">
-      <img src="/assets/logos/logo-light.svg" alt="Afrianska Logo">
+      <img src="./assets/logos/logo-light.svg" alt="Afrianska Logo">
     </a>
     <nav>
       <ul>
         <li>
-          <img class="burger-icon" src="/assets/burger-list.svg" alt="Burger List Icon">
+          <img class="burger-icon" src="./assets/burger-list.svg" alt="Burger List Icon">
         </li>
       </ul>
     </nav>
